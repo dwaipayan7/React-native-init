b6ba1fb6480a7e0db5727937efb5fc00516cd7d1
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-07-25 14:53:08 +0530

    Implement form functionality in App component with text input and submit button

--- ReactNative/App.tsx
+++ ReactNative/App.tsx
@@ -556,7 +556,73 @@
 // })
 
 
+import { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
+
+const App = () => {
+
+  const [text, setText] = useState('')
+  const [submittedText, setsubmittedText] = useState('')
+
+  console.log('====================================');
+  console.log(text);
+  console.log('====================================');
+
+
+  const handleSubmit = () => {
+    setsubmittedText(text)
+    setText('')
+  }
+
+  return (
+    <View style = {styles.container}>
+      <Text style = {styles.title}>Dwaipayan Biswas</Text>
+
+      <TextInput
+      placeholder='Enter your name'
+      style = {styles.input}
+      value={text}
+      onChangeText={setText}
+      multiline
+      numberOfLines={1}
+      />
+
+      <Button
+      title='SUBMIT'
+      onPress={handleSubmit}
+       />
+
+     { submittedText ?  (<Text>Results: {submittedText}</Text>) : null }
+
+    </View>
+  )
+}
+
+export default App
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    fontWeight: 'bold',
+    gap: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '500'
+  },
+  input: {
+    width: '100%',
+    padding: 5,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  }
+})
 
 
 
